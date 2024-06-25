1a3055cb99b753dfb0b8e9242a87e7f6bc29c04a
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-25 19:24:54 -0300

    feature: add post options

--- components/add-post/index.vue
+++ components/add-post/index.vue
@@ -12,7 +12,7 @@
       <!-- TEXT INPUT -->
       <div class="flex gap-4">
         <textarea
-          class="bg-slate-100 rounded-lg flex-1 p-2"
+          class="bg-slate-100 rounded-lg flex-1 p-2 resize-none"
           placeholder="What's on your mind?"
         > </textarea>
         <img
@@ -23,7 +23,44 @@
         />
       </div>
       <!-- POST OPTIONS -->
-      <div></div>
+      <div class="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+        <div class="flex items-center gap-2 cursor-pointer">
+          <img
+            src="/addimage.png"
+            class="w-5 h-5 cursor-pointer self-end"
+            width="20"
+            height="20"
+          />
+          Photo
+        </div>
+        <div class="flex items-center gap-2 cursor-pointer">
+          <img
+            src="/addVideo.png"
+            class="w-5 h-5 cursor-pointer self-end"
+            width="20"
+            height="20"
+          />
+          Video
+        </div>
+        <div class="flex items-center gap-2 cursor-pointer">
+          <img
+            src="/poll.png"
+            class="w-5 h-5 cursor-pointer self-end"
+            width="20"
+            height="20"
+          />
+          Poll
+        </div>
+        <div class="flex items-center gap-2 cursor-pointer">
+          <img
+            src="/addevent.png"
+            class="w-5 h-5 cursor-pointer self-end"
+            width="20"
+            height="20"
+          />
+          Event
+        </div>
+      </div>
     </div>
   </div>
 </template>
