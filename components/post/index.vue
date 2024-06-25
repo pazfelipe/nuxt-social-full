977defb97f6a97ebf7e71a89efe452ffa91ca617
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-25 20:18:00 -0300

    feature: add poster details

---
+++ components/post/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col gap-4">
+    <!-- USER -->
+    <div class="flex items-center justify-between">
+      <div class="flex items-center gap-4">
+        <NuxtImg
+          src="https://images.pexels.com/photos/25542632/pexels-photo-25542632/free-photo-of-comida-alimento-refeicao-madeira.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          class="w-10 h-10 rounded-full"
+          width="40"
+          height="40"
+        />
+        <span class="font-medium">Cody Garza</span>
+      </div>
+      <img
+        src="/more.png"
+        width="16"
+        height="16"
+      />
+    </div>
+    <!-- DESC -->
+    <div></div>
+    <!-- INTERACTION -->
+    <div></div>
+  </div>
+</template>
