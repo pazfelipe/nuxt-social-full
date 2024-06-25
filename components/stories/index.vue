bdbdd8b9109ef5840f12a64d136132e826ba1b6f
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-25 19:04:16 -0300

    feature: create story div in the stories component

--- components/stories/index.vue
+++ components/stories/index.vue
@@ -1,5 +1,66 @@
 <template>
-  <div>
-    Stories
+  <div class="p-4 bg-white rounded-lg shadow-md overflow-scroll text-sm scrollbar-hidden">
+    <div class="flex gap-8 w-max">
+      <!-- STORY -->
+      <div class="flex flex-col items-center gap-2 cursor-pointer">
+        <NuxtImg
+          src="https://images.pexels.com/photos/25559884/pexels-photo-25559884/free-photo-of-cidade-meio-urbano-estrada-via.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          class="w-20 h-20 rounded-full ring-2"
+          :width="80"
+          :height="80"
+        />
+        <span class="font-medium">Adeline</span>
+      </div>
+      <!-- STORY -->
+      <div class="flex flex-col items-center gap-2 cursor-pointer">
+        <NuxtImg
+          src="https://images.pexels.com/photos/25559884/pexels-photo-25559884/free-photo-of-cidade-meio-urbano-estrada-via.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          class="w-20 h-20 rounded-full ring-2"
+          :width="80"
+          :height="80"
+        />
+        <span class="font-medium">Adeline</span>
+      </div>
+      <!-- STORY -->
+      <div class="flex flex-col items-center gap-2 cursor-pointer">
+        <NuxtImg
+          src="https://images.pexels.com/photos/25559884/pexels-photo-25559884/free-photo-of-cidade-meio-urbano-estrada-via.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          class="w-20 h-20 rounded-full ring-2"
+          :width="80"
+          :height="80"
+        />
+        <span class="font-medium">Adeline</span>
+      </div>
+      <!-- STORY -->
+      <div class="flex flex-col items-center gap-2 cursor-pointer">
+        <NuxtImg
+          src="https://images.pexels.com/photos/25559884/pexels-photo-25559884/free-photo-of-cidade-meio-urbano-estrada-via.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          class="w-20 h-20 rounded-full ring-2"
+          :width="80"
+          :height="80"
+        />
+        <span class="font-medium">Adeline</span>
+      </div>
+      <!-- STORY -->
+      <div class="flex flex-col items-center gap-2 cursor-pointer">
+        <NuxtImg
+          src="https://images.pexels.com/photos/25559884/pexels-photo-25559884/free-photo-of-cidade-meio-urbano-estrada-via.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          class="w-20 h-20 rounded-full ring-2"
+          :width="80"
+          :height="80"
+        />
+        <span class="font-medium">Adeline</span>
+      </div>
+      <!-- STORY -->
+      <div class="flex flex-col items-center gap-2 cursor-pointer">
+        <NuxtImg
+          src="https://images.pexels.com/photos/25559884/pexels-photo-25559884/free-photo-of-cidade-meio-urbano-estrada-via.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          class="w-20 h-20 rounded-full ring-2"
+          :width="80"
+          :height="80"
+        />
+        <span class="font-medium">Adeline</span>
+      </div>
+    </div>
   </div>
 </template>
