870995e6066ecf8440345bf462e3d2ee678ee2c9
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-25 19:13:05 -0300

    feature: add poster img

--- components/add-post/index.vue
+++ components/add-post/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    Add Post
+  <div class="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+    <!-- AVATAR -->
+    <NuxtImg
+      src="https://images.pexels.com/photos/19261803/pexels-photo-19261803/free-photo-of-cidade-meio-urbano-predios-edificios.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+      class="w-12 h-12 object-cover rounded-full"
+      width="48"
+      height="48"
+    />
+    <!-- POST -->
+    <div>
+      <!-- TEXT INPUT -->
+      <div></div>
+      <!-- POST OPTIONS -->
+      <div></div>
+    </div>
   </div>
 </template>
