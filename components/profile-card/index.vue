2f33ac35f66604fa617e3c5dadc0dc48d13d4c67
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-26 08:49:25 -0300

    refactor: fetcher real user data

--- components/profile-card/index.vue
+++ components/profile-card/index.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
     <div class="h-20 relative">
-      <NuxtImg
-        src="https://images.pexels.com/photos/26575985/pexels-photo-26575985/free-photo-of-madeira-panorama-vista-paisagem.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+      <img
+        :src="user?.cover || '/noCover.png'"
         fit="fill"
         class="rounded-md object-cover h-full w-full"
       />
-      <NuxtImg
-        src="https://images.pexels.com/photos/25745076/pexels-photo-25745076/free-photo-of-oculos-escuros-oculos-de-sol-mulher-campo.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+      <img
+        :src="user?.avatar || '/noAvatar.png'"
         class="rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10"
         width="48"
         height="48"
       />
     </div>
     <div class="h-22 flex flex-col gap-2 items-center">
-      <span class="font-semibold">Duane Roberts</span>
+      <span class="font-semibold">{{ user?.name + " " + user?.surname }}</span>
       <div class="flex items-center gap-4">
-        <div class="flex">
+        <div
+          class="flex"
+          v-if="user?._count.followers > 0"
+        >
           <NuxtImg
             src="https://images.pexels.com/photos/25745076/pexels-photo-25745076/free-photo-of-oculos-escuros-oculos-de-sol-mulher-campo.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
             class="rounded-full object-cover w-3 h-3"
@@ -26,7 +29,9 @@
             :key="i"
           />
         </div>
-        <span class="text-sm text-gray-500">500 Followers</span>
+        <span class="text-sm text-gray-500"
+          >{{ user?._count.followers }} Followers</span
+        >
       </div>
       <button class="bg-blue-500 text-white text-sm p-2 rounded-md">
         My Profile
@@ -34,3 +39,37 @@
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { useAuth } from "vue-clerk";
+import { useFetch } from "@vueuse/core";
+
+const { userId } = useAuth();
+const user = ref<any>(null);
+
+const fetchUser = async () => {
+  if (userId) {
+    // @ts-ignore
+    const { data, error } = await useFetch("/api/user/find", {
+      method: "POST",
+      body: JSON.stringify({
+        userId: userId.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).json();
+
+    if (error.value) {
+      return console.log("Error: ", error.value);
+    }
+    user.value = data.value;
+  }
+};
+
+onMounted(async () => {
+  setTimeout(async () => {
+    await fetchUser();
+  }, 300);
+});
+</script>
