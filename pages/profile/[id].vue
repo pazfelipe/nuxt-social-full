e8dcb1e0982547ce069ee4ef37834ebe8e241a6b
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-25 22:09:43 -0300

    feature: implement user details

--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    Profile
+  <div class="flex gap-6 pt-6">
+    <div class="hidden xl:block w-[20%]">
+      <LeftMenu />
+    </div>
+    <div class="w-full lg:w-[70%] xl:w-[50%]">
+      <div class="flex flex-col gap-6">
+        <Feed />
+      </div>
+    </div>
+    <div class="hidden lg:block w-[30%]">
+      <RightMenu user-id="1234" />
+    </div>
   </div>
 </template>
 
