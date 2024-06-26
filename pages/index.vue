aea09a19cc8a99403f55ebd691bb6acca5667022
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-25 22:42:49 -0300

    chore: pass enum to props

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex gap-6 pt-6">
     <div class="hidden xl:block w-[20%]">
-      <LeftMenu />
+      <LeftMenu :page-type="PageTypes.HOME" />
     </div>
     <div class="w-full lg:w-[70%] xl:w-[50%]">
-      <div class="flex flex-col gap-6 ">
+      <div class="flex flex-col gap-6">
         <Stories />
         <AddPost />
         <Feed />
@@ -16,7 +16,9 @@
   </div>
 </template>
 
-<script lang="ts"  setup>
+<script lang="ts" setup>
+import { PageTypes } from "~/types/utils";
+
 definePageMeta({
   middleware: ["signed-in"],
 });
--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex gap-6 pt-6">
     <div class="hidden xl:block w-[20%]">
-      <LeftMenu />
+      <LeftMenu :page-type="PageTypes.PROFILE" />
     </div>
     <div class="w-full lg:w-[70%] xl:w-[50%]">
       <div class="flex flex-col gap-6">
@@ -15,6 +15,8 @@
 </template>
 
 <script setup lang="ts">
+import { PageTypes } from "~/types/utils";
+
 definePageMeta({
   middleware: ["signed-out"],
 });
