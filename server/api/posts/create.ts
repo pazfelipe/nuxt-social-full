c95af7753a3d4d3bc33db0f31f7d39eb4784c7ba
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-25 23:57:01 -0300

    feature: create endpoint to fetch post

---
+++ server/api/posts/create.ts
@@ -0,0 +1,22 @@
+// server/api/posts/create.ts
+import {prisma} from '~/server/lib/client';
+import {defineEventHandler, readBody} from 'h3';
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+
+  const {desc, userId} = body;
+
+  try {
+    const post = await prisma.post.create({
+      data: {
+        desc,
+        userId,
+      },
+    });
+    return post;
+  } catch (err) {
+    event.res.statusCode = 500;
+    return {error: 'Failed to create post'};
+  }
+});
