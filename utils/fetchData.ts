6ed5bc9076cc8de36a8d3f35195ca3f441840362
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-26 09:40:26 -0300

    refactor: return status code

--- utils/fetchData.ts
+++ utils/fetchData.ts
@@ -2,15 +2,17 @@ import type {HttpMethod} from 'svix/dist/openapi';
 import {useFetch} from "@vueuse/core";
 
 export const fetchData = async (path: string, method: HttpMethod, body: Record<any, any>): Promise<{data: any;}> => {
-  const {data, error} = await useFetch(`/api/${path}`, {
+
+  const {data, error, statusCode} = await useFetch(`/api/${path}`, {
     method,
     body: JSON.stringify(body),
     headers: {
       "Content-Type": "application/json",
     },
   }).json();
+
   if (error.value) {
-    return Promise.reject(error.value);
+    return Promise.reject({error: error.value, status: statusCode.value});
   }
   return Promise.resolve({data});
 };
