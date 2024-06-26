550ff4d47c2828ca64d3663da03d798fb9cb9295
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-25 22:10:02 -0300

    feature: implement conditional user rendering

--- components/right-menu/index.vue
+++ components/right-menu/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="flex flex-col gap-6">
+    <div v-if="userId" class="flex flex-col gap-6">
+      <UserInformationCard :user-id="userId" />
+      <UserMediaCard :user-id="userId" />
+    </div>
     <FriendRequests />
     <Birthdays />
     <Ad :size="Sizes.MEDIUM" />
@@ -7,13 +11,13 @@
 </template>
 
 <script lang="ts" setup>
-import {Sizes} from '~/types/utils';
+import { Sizes } from "~/types/utils";
 
 const props = defineProps({
   userId: {
     type: String,
-    required: false
-  }
+    required: false,
+  },
 });
 
 const userId = computed(() => props.userId);
