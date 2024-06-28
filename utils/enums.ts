bdf3adcc414d87d2cd507c33514065d327912fbe
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-27 23:16:56 -0300

    refactor: move enum file to utils folder

---
+++ utils/enums.ts
@@ -0,0 +1,8 @@
+export enum SERVER_USER_ACTIONS {
+  FIND = 'findUserByUsername',
+  FOLLOW_REQUEST = 'followRequest',
+  FOLLOWER = 'follower',
+  IS_BLOCKED = 'isBlocked',
+  SWITCH_BLOCK = 'switchBlock',
+  SWITCH_FOLLOW = 'switchFollow'
+}
