3942d7ff94689bf3b01c78d8be9b487204cbc2d9
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-28 14:12:36 -0300

    feature: create component to display following requests

---
+++ components/friend-requests/request-list.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="flex items-center justify-between"
+    v-for="request in props.requests"
+  >
+    <div class="flex items-center gap-4">
+      <NuxtImg
+        :src="request.sender.avatar"
+        width="40"
+        height="40"
+        class="w-10 h-10 rounded-full object-cover"
+      />
+      <span class="font-semibold">
+        {{ request.sender.name }} {{ request.sender.surname }}</span
+      >
+    </div>
+    <div class="flex gap-3 justify-end">
+      <img
+        src="/accept.png"
+        alt=""
+        width="20"
+        height="20"
+        class="cursor-pointer"
+      />
+      <img
+        src="/reject.png"
+        alt=""
+        width="20"
+        height="20"
+        class="cursor-pointer"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  requests: {
+    type: Object,
+  },
+});
+</script>
